207e36f7940f4c63aa25f7bc3410577494de774f
sunsuke <nisemono12041141@gmail.com>
2020-11-20 15:00:48 +0900

    コントローラ作成

--- core/Controller.php
+++ core/Controller.php
@@ -12,11 +12,23 @@ abstract class Contoller
     public function __contruct()
     {
         $this->controller_name = strtolower( substr ( get_class($this) , 0 , 10 ));
-        
+
         $this->application = $application;
         $this->request = $application->getRequest();
         $this->response = $application->getResponse();
         $this->session = $application->getSession();
         $this->db_manager = $application->getDbManager();
+    }
+
+    public function run($action , $params = array())
+    {
+        $this->action_name = $action;
+        $action_method = $action . 'Action';
+        if(!method_exist($this , $action_method)){
+            $this->forward404();
+        }
+        $content = $this->$action_method($params);
+
+        return $content;
     }
 }
